cc4ae6c11445b69d36f79bb945e1983b084a0f5f
lpmconta01 <lpmconta01@lpmconta.org>
2023-12-01 13:46:48 -0500

    fixSelectColegio

--- src/app/web-pro/pages/flujo-principal/select-colegio/select-colegio.component.ts
+++ src/app/web-pro/pages/flujo-principal/select-colegio/select-colegio.component.ts
@@ -16,40 +16,22 @@ export class SelectColegioComponent implements OnInit {
 
   arrayProcesos: Flujo[] = [
     {
-      title: 'Acceder a planilla Manual',
+      title: 'Asignar Empresa',
       description:
-        'Se accede de manera similar a la planilla Mensual, solo que aquí podremos hacer modificaciones específicas',
-      urlImage: 'assets/Flujo-principal/Procesos/1.acceder.png',
+        'Aquí puedes seleccionar el colegio u organización que necesites, basta con dar un click y todo estará listo.',
+      urlImage: 'assets/Flujo-principal/Empresa/1.asignar.png',
     },
     {
       title: 'Verificar conceptos disponible',
       description:
         'Si tenemos subsidios, vacaciones, tardanzas etc. ( no aplica para remuneración mensual de trabajador ni horext), estos conceptos apareceran en columnas, también aplica para algunos descuentos. ',
-      urlImage: 'assets/Flujo-principal/Procesos/2.verificar.png',
+      urlImage: 'assets/Flujo-principal/Empresa/2.seleccionar.png',
     },
     {
-      title: 'Modificar montos y guardar',
+      title: 'Verificar si el colegio se seleccionó',
       description:
-        'Una vez encontradas las columnas a modificar, borramos el monto y colocamos el nuestro, procedemos a guardar',
-      urlImage: 'assets/Flujo-principal/Procesos/3.modificar.png',
-    },
-    {
-      title: 'Ingresar a planilla Mensual',
-      description:
-        'Salimos y ahora ingresamos a la planilla Mensual de siempre.',
-      urlImage: 'assets/Flujo-principal/Procesos/4.mensual.png',
-    },
-    {
-      title: 'Procesar',
-      description:
-        'Procesamos a los trabajadores que modificamos desde la planilla manual, es recomendable procesar individualmente.',
-      urlImage: 'assets/Flujo-principal/Procesos/5.procesar.png',
-    },
-    {
-      title: 'Regresar valor a la normalidad',
-      description:
-        'Este paso es opcional, si queremos regresar a su valor original, repetimos el procedimiento de ingresar a planilla Manual pero ahora en el campo a modificar colocamos 0 y guardamos. Posteriormente regresamos a Mensual y volvemos a procesar',
-      urlImage: 'assets/Flujo-principal/Procesos/6.normal.png',
+        'En la cabecera de la planilla podemos observar la organización y el mes en el que nos encontramos.',
+      urlImage: 'assets/Flujo-principal/Empresa/3.verificar.png',
     },
   ];
 }
